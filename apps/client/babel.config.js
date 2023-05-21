9730743492d379f3d399a2dbced0b8c4daca222a
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-05-21 18:58:51 +0530

    init: remove configs for absolute import > configs werent working as intended

--- apps/client/babel.config.js
+++ apps/client/babel.config.js
@@ -2,33 +2,8 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: ["babel-preset-expo"],
-		env: {
-			production: {
-				plugins: ["react-native-paper/babel"],
-			},
-		},
 		plugins: [
 			require.resolve("expo-router/babel"),
-			// Removable in Expo SDK 49
-			[
-				"module-resolver",
-				{
-					extensions: [
-						".ts",
-						".tsx",
-						".android.ts",
-						".android.tsx",
-						".ios.ts",
-						".ios.tsx",
-					],
-					alias: {
-						src: "./src",
-						"src/types": ["./src/types"],
-						"src/types/redux": ["./src/redux/types"],
-						"src/svg": ["./src/assets/svg/index"],
-					},
-				},
-			],
 			"@babel/plugin-proposal-export-namespace-from", // plugin to remove reanimated error -> https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#web
 			"react-native-reanimated/plugin",
 		],
