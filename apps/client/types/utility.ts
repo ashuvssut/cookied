49d8883604701e2d0fc5bcbb4b83676c55accb42
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-05-21 21:46:24 +0530

    feat: add utility types

---
+++ apps/client/types/utility.ts
@@ -0,0 +1,10 @@
+// https://stackoverflow.com/questions/37688318/typescript-interface-possible-to-make-one-or-the-other-properties-required
+type Only<T, U> = {
+	[P in keyof T]: T[P];
+} & {
+	[P in keyof U]?: never;
+};
+
+export type Either<T, U> = Only<T, U> | Only<U, T>;
+
+export type PickPartial<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>> & Partial<Pick<T, K>> 
