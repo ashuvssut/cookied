def6a63549dfaf4e5da577cac98d61876b308bc6
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-05-22 12:56:42 +0530

    init: chrome extension assets, MUI & configs

--- apps/chrome-extension/manifest.ts
+++ apps/chrome-extension/manifest.ts
@@ -15,13 +15,13 @@ const manifest: chrome.runtime.ManifestV3 = {
   },
   action: {
     default_popup: "src/pages/popup/index.html",
-    default_icon: "icon-34.png",
+    default_icon: "34-logo.png",
   },
   chrome_url_overrides: {
     newtab: "src/pages/newtab/index.html",
   },
   icons: {
-    "128": "icon-128.png",
+    "128": "128-logo.png",
   },
   content_scripts: [
     {
@@ -31,14 +31,14 @@ const manifest: chrome.runtime.ManifestV3 = {
       css: ["assets/css/contentStyle<KEY>.chunk.css"],
     },
   ],
-  devtools_page: "src/pages/devtools/index.html",
+  // devtools_page: "src/pages/devtools/index.html",
   web_accessible_resources: [
     {
       resources: [
         "assets/js/*.js",
         "assets/css/*.css",
-        "icon-128.png",
-        "icon-34.png",
+        "128-logo.png",
+        "34-logo.png",
       ],
       matches: ["*://*/*"],
     },
