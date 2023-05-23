c2e17abec58253a390ee9820a6f9e1819f7a49c4
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-05-23 08:55:42 +0530

    feat better types for useSendIntent chore: update workspace configs to open terminal in workspace root

--- apps/client/hooks/useSendIntent.ts
+++ apps/client/hooks/useSendIntent.ts
@@ -1,14 +1,13 @@
 import { useCallback, useEffect, useState } from "react";
-import ShareMenu from "react-native-share-menu";
-import { ShareData } from "react-native-share-menu";
+import ShareMenu, { ShareData } from "react-native-share-menu";
 import { LogBox } from "react-native";
 
 export const useSendIntent = () => {
-	const [sharedData, setSharedData] = useState<string | string[]>("");
-	const [sharedMimeType, setSharedMimeType] = useState("");
-	const [sharedExtraData, setSharedExtraData] = useState<object | undefined>(
-		undefined,
-	);
+	const [sharedData, setSharedData] = useState<ShareData["data"]>("");
+	const [sharedMimeType, setSharedMimeType] =
+		useState<ShareData["mimeType"]>("");
+	const [sharedExtraData, setSharedExtraData] =
+		useState<ShareData["extraData"]>(undefined);
 
 	const handleShare = useCallback((item?: ShareData) => {
 		if (!item) return;
@@ -20,17 +19,13 @@ export const useSendIntent = () => {
 		setSharedMimeType(mimeType);
 	}, []);
 
-	useEffect(() => {
-		ShareMenu.getInitialShare(handleShare);
-	}, []);
+	useEffect(() => void ShareMenu.getInitialShare(handleShare), []);
 
 	useEffect(() => {
 		LogBox.ignoreLogs(["new NativeEventEmitter()"]);
 		const listener = ShareMenu.addNewShareListener(handleShare);
 
-		return () => {
-			listener.remove();
-		};
+		return () => listener.remove();
 	}, []);
 
 	return { sharedData, sharedMimeType, sharedExtraData };
