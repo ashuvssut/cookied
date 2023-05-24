30e49655ee2e77a65f0c57377f0210381257b372
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-05-24 11:43:54 +0530

    init(api):  init appwrite fx workspace w/ scripts

---
+++ apps/api/scripts.js
@@ -0,0 +1,36 @@
+import { execSync } from "child_process";
+import fs from "fs";
+
+function editPackageJsons() {
+	const packageJsons = execSync("find functions -name package.json");
+
+	const packageJsonsArray = packageJsons.toString().split("\n");
+
+	packageJsonsArray.forEach(packageJsonPath => {
+		if (!packageJsonPath) return;
+		const pwd = packageJsonPath.split("/")[1];
+
+		const kebabCasePwd = pwd.trim().replace(/\s+/g, "-").toLowerCase();
+
+		const packageJson = fs.readFileSync(packageJsonPath, "utf8");
+
+		const newPackageJson = packageJson.replace(
+			/\"name\": \"[^\"]*\"/g,
+			`"name": "${kebabCasePwd}"`,
+		);
+
+		fs.writeFileSync(packageJsonPath, newPackageJson);
+
+		console.log(`Updated ${packageJsonPath}`);
+	});
+}
+
+function convertToKebabCase(str) {
+	const str = str.trim();
+	// insert a space in camelCase text such as "camelCase" to "camel Case"
+	const insertSpace = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+	const lowerCase = insertSpace.toLowerCase();
+	return lowerCase.replace(/(\s|_)+/g, "-");
+}
+
+editPackageJsons();
