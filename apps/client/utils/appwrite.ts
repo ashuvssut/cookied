9b4a82a12bfaf86edef79a6d6c694d6bf33b6a16
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-05-28 18:34:17 +0530

    fix(client): env vars setting error msg correction

--- apps/client/utils/appwrite.ts
+++ apps/client/utils/appwrite.ts
@@ -3,16 +3,16 @@ import Constants from "expo-constants";
 
 export const APPWRITE_ENDPOINT = Constants.expoConfig?.extra?.APPWRITE_ENDPOINT;
 if (!APPWRITE_ENDPOINT)
-	throw Error("APPWRITE_ENDPOINT is not set in .env (Refer .env.example)");
+	throw Error("APPWRITE_ENDPOINT isn't set in env.js (Refer env.example.js)");
 
 export const APPWRITE_PROJECT_ID =
 	Constants.expoConfig?.extra?.APPWRITE_PROJECT_ID;
 if (!APPWRITE_PROJECT_ID)
-	throw Error("APPWRITE_PROJECT_ID is not set in .env (Refer .env.example)");
+	throw Error("APPWRITE_PROJECT_ID isn't set in env.js (Refer env.example.js)");
 
 const VERCEL_DEV_ENDPOINT = Constants.expoConfig?.extra?.VERCEL_DEV_ENDPOINT;
 if (!VERCEL_DEV_ENDPOINT)
-	throw Error("VERCEL_DEV_ENDPOINT is not set in .env (Refer .env.example)");
+	throw Error("VERCEL_DEV_ENDPOINT isn't set in env.js (Refer env.example.js)");
 
 console.log(JSON.stringify(Constants.expoConfig?.extra, null, 2));
 
