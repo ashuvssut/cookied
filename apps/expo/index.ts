2bad455faed738ef6dedf21aee019d53ed3da4f8
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-06-01 00:26:26 +0530

    update .gitignore +logbox hide expo constants warn

--- apps/expo/index.ts
+++ apps/expo/index.ts
@@ -1,2 +1,7 @@
 // registerRootComponent happens in "expo-router/entry"
 import 'expo-router/entry'
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs([
+	"Constants.manifest has been deprecated in favor of Constants.expoConfig.",
+]);
