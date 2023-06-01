97ad96f6eb12feb5dedf62820cce3c97a3a5fa7d
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-06-01 16:02:13 +0530

    build: expo-image + other configs for web

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -3,7 +3,6 @@ const env = require('./env')
 module.exports = {
   slug: 'Cookied',
   name: 'Cookied',
-  version: '0.1.0',
   scheme: 'cookied',
   assetBundlePatterns: ['**/*'],
   version: '1.0.0',
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
-const { withExpo } = require('@expo/next-adapter')
+const { withExpo } = require("@expo/next-adapter");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -20,8 +20,8 @@ const nextConfig = {
 		"@expo/html-elements",
 		"react-native-gesture-handler",
 		"expo-modules-core",
-    "expo-constants",
+		"expo-constants",
 	],
 };
 
-module.exports = withExpo(nextConfig)
+module.exports = withExpo(nextConfig);
