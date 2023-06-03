838a91d63abaa5a8cd1fc06d5f5e8a183e32b8f4
Md Arshad Khan <adrenine13@gmail.com>
2023-06-04 00:35:14 +0530

    Refactored redux provider

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -3,22 +3,21 @@ import { Slot } from "expo-router";
 import LoadingModal from "app/components/LoadingModal";
 import { PersistGate } from "redux-persist/integration/react";
 
-import ReduxProvider from "app/redux/store/ReduxProvider.native";
 import { ProtectedRoute } from "app/components/ProtectedRoute";
 import { DripsyTheme } from "app/theme";
-import { nativeStore } from "app/redux/store";
+import { nativeStore, NativeReduxProvider } from "app/redux/store";
 
 export default function Root() {
 	return (
 		<DripsyTheme>
-			<ReduxProvider>
+			<NativeReduxProvider>
 				<PersistGate loading={null} persistor={nativeStore().persistor}>
 					<ProtectedRoute>
 						<Slot />
 					</ProtectedRoute>
 					<LoadingModal />
 				</PersistGate>
-			</ReduxProvider>
+			</NativeReduxProvider>
 		</DripsyTheme>
 	);
 }
