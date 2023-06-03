90c01d6b539fb29c2de39bc1e9ad6cc765175ef4
Md Arshad Khan <adrenine13@gmail.com>
2023-06-04 01:08:25 +0530

    Removed reduxProvider cyclic calls

--- packages/app/redux/store/ReduxProvider.native.tsx
+++ packages/app/redux/store/ReduxProvider.native.tsx
@@ -1,10 +1,8 @@
 // TODO :-Persist Gate
 import React from "react";
-import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 
-import { nativeStore } from "./index";
-import { Platform } from "react-native";
+import  nativeStore  from "./store.native";
 
 function ReduxProvider({ children }: { children: React.ReactNode }) {
 	return (
