62b19f15c5d7420b85830ab447847cbe25a9b27d
Md Arshad Khan <adrenine13@gmail.com>
2023-06-03 16:25:27 +0530

    Added plugin for flipper

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -1,18 +1,28 @@
-const env = require('./env')
+const env = require("./env");
 
 module.exports = {
-  slug: 'Cookied',
-  name: 'Cookied',
-  version: '0.1.0',
-  scheme: 'cookied',
-  assetBundlePatterns: ['**/*'],
-  version: '1.0.0',
-  extra: {
-    ...env,
-    MODE: !!env.MODE ? env.MODE : 'dev',
-  },
-  web: { bundler: 'metro' },
-  platforms: ['ios', 'android'],
-  ios: { bundleIdentifier: 'com.cookied' },
-  android: { package: 'com.cookied' },
-}
+	slug: "Cookied",
+	name: "Cookied",
+	version: "0.1.0",
+	scheme: "cookied",
+	assetBundlePatterns: ["**/*"],
+	version: "1.0.0",
+	extra: {
+		...env,
+		MODE: !!env.MODE ? env.MODE : "dev",
+	},
+	web: { bundler: "metro" },
+	platforms: ["ios", "android"],
+	ios: { bundleIdentifier: "com.cookied" },
+	android: { package: "com.cookied" },
+	plugins: [
+		[
+			"expo-build-properties",
+			{
+				ios: {
+					flipper: true,
+				},
+			},
+		],
+	],
+};
