f29201eede598269bd5fc7db6288af0bcca82fdc
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-06-04 13:34:37 +0530

    feat(app): persisted storages clear util

--- packages/app/store/ReduxProvider.tsx
+++ packages/app/store/ReduxProvider.tsx
@@ -3,8 +3,10 @@ import { FCC } from "app/types/IReact";
 import { PersistGate } from "redux-persist/integration/react";
 import LoadingModal from "app/components/LoadingModal";
 import { persistor, store } from "app/store/store";
+import { resetPersistedStorages } from "app/utils/storage";
 
 export const ReduxProvider: FCC = ({ children }) => {
+	// resetPersistedStorages();
 	return (
 		<Provider store={store}>
 			{/* @ts-ignore */}
--- packages/app/utils/storage.ts
+++ packages/app/utils/storage.ts
@@ -2,6 +2,8 @@
 import { atomWithStorage, createJSONStorage } from "jotai/utils";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from "react-native";
+import { persistStore } from "redux-persist";
+import { store } from "app/store/store";
 
 // content: anything JSON serializable
 export function atomWithAsyncStorage<T>(key: string, content: T) {
@@ -17,4 +19,11 @@ export function atomWithLocalStorage<T>(key: string, content: T) {
 export function atomWithPlatformStorage<T>(key: string, content: T) {
 	if (Platform.OS === "web") return atomWithLocalStorage(key, content);
 	else return atomWithAsyncStorage(key, content);
+}
+
+export function resetPersistedStorages() {
+	if (Platform.OS === "web") localStorage.clear();
+	else AsyncStorage.clear();
+
+	persistStore(store).purge();
 }
