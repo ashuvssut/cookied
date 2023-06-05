6d47e2792f4249f5241f566ecc55ec22097ff271
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-06-05 13:49:48 +0530

    build(native): upg flipper+add async storage ext

--- apps/expo/index.ts
+++ apps/expo/index.ts
@@ -1,7 +1,9 @@
 // registerRootComponent happens in "expo-router/entry"
-import 'expo-router/entry'
-import { LogBox } from 'react-native';
+import "expo-router/entry";
+import { LogBox } from "react-native";
+import { initAsyncStorageInspector } from "asyncstorage-inspector-flipper";
 
+initAsyncStorageInspector();
 LogBox.ignoreLogs([
 	"Constants.manifest has been deprecated in favor of Constants.expoConfig.",
 ]);
