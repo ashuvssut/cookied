ff7cd7d70db66fda6a86d86ec65495e6672ac814
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-06-08 22:05:49 +0530

    refactor: refactored and typed bookmark apis

---
+++ packages/app/apis/utils/factoryFunctions.ts
@@ -0,0 +1,14 @@
+import { Models } from "appwrite";
+
+
+export function cleanResponse<T extends Models.Document>(response: T) {
+	const { $collectionId, $permissions, $userId, $databaseId, ...rest } =
+		response;
+	return rest;
+}
+
+export function cleanResponseIterative<
+	T extends Models.DocumentList<Models.Document>,
+>(response: T) {
+	return response.documents.map(res => cleanResponse(res));
+}
