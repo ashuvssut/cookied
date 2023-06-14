72140d8a39e0fdc06a139712fa55b79299d6e251
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-06-14 22:58:49 +0530

    feat: add eas build configs

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -1,22 +1,23 @@
 const env = require("./env");
 
 module.exports = {
-	slug: "Cookied",
+	slug: "cookied",
 	name: "Cookied",
+	displayName: "Cookied",
 	version: "0.1.0",
 	scheme: "cookied",
 	assetBundlePatterns: ["**/*"],
+	owner: env.EAS_PROJECT_OWNER,
 	extra: {
 		...env,
-		"eas": {
-			"projectId": "42c6beb7-240e-4c27-bab8-a48661494de3"
-		}
+		eas: {
+			projectId: env.EAS_PROJECT_ID,
+		},
 		// MODE: !!env.MODE ? env.MODE : "dev", // example
 	},
 	web: { bundler: "metro" },
 	platforms: ["ios", "android"],
 	ios: { bundleIdentifier: "com.cookied" },
-	android: { package: "com.cookied" },
 	plugins: [
 		[
 			"expo-build-properties",
