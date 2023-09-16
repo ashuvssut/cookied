19b3f70a66c12610a9fe4727bc1767d553d98bb9
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-09-16 16:41:23 +0530

    feat: expo-secure-store for auth in native

--- packages/app/components/ClerkAuth/ClerkAuth.tsx
+++ packages/app/components/ClerkAuth/ClerkAuth.tsx
@@ -3,11 +3,35 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 import { FCC } from "app/types/IReact";
 import { CLERK_PUBLISHABLE_KEY, CONVEX_URL } from "app/utils/env";
+import * as SecureStore from "expo-secure-store";
+import { Toast } from "app/components/Toast";
+
+const tokenCache = {
+	async getToken(key: string) {
+		try {
+			return SecureStore.getItemAsync(key);
+		} catch (err) {
+			Toast.error(err.message || err.toString());
+			return;
+		}
+	},
+	async saveToken(key: string, value: string) {
+		try {
+			return SecureStore.setItemAsync(key, value);
+		} catch (err) {
+			Toast.error(err.message || err.toString());
+			return;
+		}
+	},
+};
 
 const convex = new ConvexReactClient(CONVEX_URL);
 export const ClerkAuth: FCC = ({ children }) => {
 	return (
-		<ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+		<ClerkProvider
+			tokenCache={tokenCache}
+			publishableKey={CLERK_PUBLISHABLE_KEY}
+		>
 			<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
 				{children}
 			</ConvexProviderWithClerk>
