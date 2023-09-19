29f1aeee8de9b22386aa640c1b69122e39c3652e
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-09-19 21:55:43 +0530

    build: yarn workspaces config

--- apps/chrome-extension/utils/reload/constant.ts
+++ apps/chrome-extension/utils/reload/constant.ts
@@ -1,4 +1,4 @@
-export const LOCAL_RELOAD_SOCKET_PORT = 8081;
+export const LOCAL_RELOAD_SOCKET_PORT = 1338;
 export const LOCAL_RELOAD_SOCKET_URL = `ws://localhost:${LOCAL_RELOAD_SOCKET_PORT}`;
 export const UPDATE_PENDING_MESSAGE = "wait_update";
 export const UPDATE_REQUEST_MESSAGE = "do_update";
