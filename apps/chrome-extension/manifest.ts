f738478ae12530290b9d469f64302e51d4ef1df6
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-09-19 22:09:21 +0530

    feat: chrome-extension basic setup

--- apps/chrome-extension/manifest.ts
+++ apps/chrome-extension/manifest.ts
@@ -5,24 +5,24 @@ import packageJson from "./package.json";
  */
 const manifest: chrome.runtime.ManifestV3 = {
   manifest_version: 3,
-  name: packageJson.name,
+  name: "Cookied!!",
   version: packageJson.version,
   description: packageJson.description,
   permissions: ["storage"],
-  options_page: "src/pages/options/index.html",
-  background: {
-    service_worker: "src/pages/background/index.js",
-    type: "module",
-  },
+  // options_page: "src/pages/options/index.html",
+  // background: {
+  //   service_worker: "src/pages/background/index.js",
+  //   type: "module",
+  // },
   action: {
     default_popup: "src/pages/popup/index.html",
-    default_icon: "icon-34.png",
+    default_icon: "34-logo.png",
   },
   chrome_url_overrides: {
     newtab: "src/pages/newtab/index.html",
   },
   icons: {
-    "128": "icon-128.png",
+    "128": "128-logo.png",
   },
   content_scripts: [
     {
@@ -32,14 +32,14 @@ const manifest: chrome.runtime.ManifestV3 = {
       css: ["assets/css/contentStyle<KEY>.chunk.css"],
     },
   ],
-  devtools_page: "src/pages/devtools/index.html",
+  // devtools_page: "src/pages/devtools/index.html",
   web_accessible_resources: [
     {
       resources: [
         "assets/js/*.js",
         "assets/css/*.css",
-        "icon-128.png",
-        "icon-34.png",
+        "128-logo.png",
+        "34-logo.png",
       ],
       matches: ["*://*/*"],
     },
