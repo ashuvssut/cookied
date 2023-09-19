cd1e5f00f8f36f7a42cb9187db73aad39b1d7bd3
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-09-20 03:16:09 +0530

    feat: User detail w/ Avatar +logout  (ProfileMenu)

--- packages/app/components/Header.tsx
+++ packages/app/components/Header.tsx
@@ -1,46 +1,40 @@
 import { GoodCookie } from "app/assets/svg";
 import { Svg } from "app/components/Svg";
 import { H1, View, Pressable, Text } from "dripsy";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Bar } from "react-native-progress";
 import { atom, useAtom } from "jotai";
 import { isWeb } from "app/utils/constants";
-import { useAuth } from "app/utils/clerk";
+import { useAuth, useUser } from "app/utils/clerk";
 import { Toast } from "app/components/Toast";
 import { useRouter } from "solito/router";
+import { Avatar } from "app/components/Avatar";
+import { Th } from "app/theme/components";
 
 export const barLoadingAtom = atom(false);
 export const Header: FC = () => {
 	const [loading] = useAtom(barLoadingAtom);
-	const { isLoaded, signOut } = useAuth();
-	const router = useRouter();
+	const { isLoaded } = useAuth();
 
 	return (
-		<View sx={{ justifyContent: "center", alignItems: "center" }}>
-			<View variant="layout.row" sx={{ gap: 5 }}>
-				<Svg
-					Svg={GoodCookie}
-					commonSvgProps={{ height: 30, width: 30, style: { top: -2 } }}
-				/>
-				<H1>COOKIED</H1>
+		<View sx={{ justifyContent: "center", alignItems: "center", zIndex: 1 }}>
+			<View
+				sx={{
+					flexDirection: "row",
+					justifyContent: "space-between",
+					width: "100%",
+					px: "$4",
+				}}
+			>
+				<View variant="layout.row" sx={{ gap: 5 }}>
+					<Svg
+						Svg={GoodCookie}
+						commonSvgProps={{ height: 30, width: 30, style: { top: -2 } }}
+					/>
+					<H1>COOKIED</H1>
+				</View>
+				{isWeb && isLoaded && <ProfileMenuDisplay />}
 			</View>
-			{isWeb && isLoaded && (
-				<Pressable
-					onPress={async () => {
-						try {
-							await signOut();
-						} catch (err) {
-							Toast.error("Unable to Log out.");
-							console.log(err.message || err.toString());
-						}
-						router.replace({ pathname: "/" });
-					}}
-					variant="layout.center"
-					sx={{ position: "absolute", top: 25, right: 15 }}
-				>
-					<Text>LOGOUT</Text>
-				</Pressable>
-			)}
 			<Bar
 				indeterminate={true}
 				width={null}
@@ -52,6 +46,62 @@ export const Header: FC = () => {
 				}}
 				height={3}
 			/>
+		</View>
+	);
+};
+
+const ProfileMenuDisplay = () => {
+	const { user } = useUser();
+	if (!user) return null;
+
+	const [showMenu, setShowMenu] = useState(false);
+	return (
+		<View>
+			<View
+				sx={{ flexDirection: "row", gap: 10, py: 10, cursor: "pointer" }}
+				onClick={() => setShowMenu(prev => !prev)}
+			>
+				<Avatar />
+				<View sx={{ justifyContent: "center" }}>
+					<Text variant="semibold" sx={{ fontSize: 22, lineHeight: 30 }}>
+						Hi
+					</Text>
+					<Text variant="semibold">{user?.firstName}</Text>
+				</View>
+			</View>
+			<View sx={{ position: "absolute", bottom: 0, right: 0, width: "100%" }}>
+				<ProfileMenu showMenu={showMenu} />
+			</View>
+		</View>
+	);
+};
+
+const ProfileMenu = ({ showMenu }: { showMenu: boolean }) => {
+	const { signOut } = useAuth();
+	const router = useRouter();
+
+	if (!showMenu) return null;
+	return (
+		<View sx={{ position: "absolute", top: 0, width: "100%", height: 80 }}>
+			<View
+				sx={{ bg: "surfaceHigh", height: "100%", borderRadius: 20, px: 10 }}
+			>
+				<Th.ButtonSecondary
+					onPress={async () => {
+						try {
+							await signOut();
+						} catch (err) {
+							Toast.error("Unable to Log out.");
+							console.log(err.message || err.toString());
+						}
+						router.replace({ pathname: "/" });
+					}}
+					variant="layout.center"
+					sx={{}}
+				>
+					<Text>LOGOUT</Text>
+				</Th.ButtonSecondary>
+			</View>
 		</View>
 	);
 };
