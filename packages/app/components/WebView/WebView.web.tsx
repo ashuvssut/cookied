658ee723c29f95a8208b1df653ee42ac325368db
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-09-23 21:56:24 +0530

    failed try

--- packages/app/components/WebView/WebView.web.tsx
+++ packages/app/components/WebView/WebView.web.tsx
@@ -1,5 +1,12 @@
 import { View } from "dripsy";
-import { DetailedHTMLProps, FC, IframeHTMLAttributes } from "react";
+import {
+	DetailedHTMLProps,
+	FC,
+	IframeHTMLAttributes,
+	useEffect,
+	useState,
+} from "react";
+import { Toast } from "app/components/Toast";
 
 interface IWebView
 	extends DetailedHTMLProps<
@@ -7,13 +14,38 @@ interface IWebView
 		HTMLIFrameElement
 	> {}
 export const WebView: FC<IWebView> = ({ src, style, ...props }) => {
+	const [htmlContent, setHtmlContent] = useState<string | undefined>(undefined);
+
+	useEffect(() => {
+		async function getHtml() {
+			if (!src) return;
+			try {
+				const res = await fetch(
+					"http://localhost:2023/api/webPageFetch?url=" + src,
+				);
+
+				if (res.status === 200) {
+					const json = await res.json();
+					setHtmlContent(json.body);
+				} else {
+					console.error(`fetch webpage failed. Status: ${res.status}`);
+					Toast.error(`Something went wrong! Failed to fetch webpage`);
+				}
+			} catch (error) {
+				console.error("Error fetching HTML content:", error);
+			}
+		}
+
+		getHtml();
+	}, [src]);
+
 	return (
 		<View variant="layout.absoluteFlex">
 			<iframe
 				style={{ border: "none", ...style }}
 				loading="eager"
 				sandbox="allow-modals"
-				src={src}
+				srcDoc={htmlContent || ""}
 				{...props}
 			/>
 		</View>
