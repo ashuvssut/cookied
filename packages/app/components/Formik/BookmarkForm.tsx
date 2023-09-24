ed559f13d4deeda5159ba7d5598c407716097704
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-09-24 09:21:11 +0530

    feat: UX: in add Bm form URL is the first field

--- packages/app/components/Formik/BookmarkForm.tsx
+++ packages/app/components/Formik/BookmarkForm.tsx
@@ -61,15 +61,15 @@ export const BookmarkForm: FC<IBookmarkForm> = ({ formikProps: p }) => {
 	return (
 		<>
 			<FormTextField
-				name="title"
+				name="url"
 				formikProps={p}
-				fieldProps={{ placeholder: "Enter the Bookmark name", autoFocus: true }}
+				fieldProps={{ placeholder: "Enter the Bookmark URL", autoFocus: true }}
 			/>
 			<View sx={{ marginTop: "$4" }} />
 			<FormTextField
-				name="url"
+				name="title"
 				formikProps={p}
-				fieldProps={{ placeholder: "Enter the Bookmark URL" }}
+				fieldProps={{ placeholder: "Enter the Bookmark name" }}
 			/>
 			<BmFolderPathSearch
 				formikProps={p}
