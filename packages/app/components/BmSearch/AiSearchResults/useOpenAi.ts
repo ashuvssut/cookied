96fb92308ebe132d541bbdc9063b77df8f67457f
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-09-27 18:23:28 +0530

    feat: validateOpenAiKey + discard key on logout

--- packages/app/components/BmSearch/AiSearchResults/useOpenAi.ts
+++ packages/app/components/BmSearch/AiSearchResults/useOpenAi.ts
@@ -1,7 +1,12 @@
-import { getEncryptedKey, storeEncryptedKey } from "app/components/BmSearch/AiSearchResults/openAi";
+import {
+	getEncryptedKey,
+	storeEncryptedKey,
+} from "app/components/BmSearch/AiSearchResults/openAi";
+import { barLoadingAtom } from "app/components/Header";
 import { Toast } from "app/components/Toast";
 import { useAction } from "convex/react";
 import { api } from "gconvex/_generated/api";
+import { useAtom } from "jotai";
 import { useEffect, useState } from "react";
 
 export function useOpenAi() {
@@ -21,17 +26,47 @@ export function useOpenAi() {
 		getKey();
 	}, []);
 
+	const [, setLoading] = useAtom(barLoadingAtom);
 	const encryptOpenAiKey = useAction(api.openAi.encryptOpenAiKey);
 	async function submitApiKey(key: string) {
 		try {
-			const encKey = await encryptOpenAiKey({ openAiKey: key });
-			if (typeof encKey === "string" && !!encKey) setEncryptedKey(encKey);
+			setLoading(true);
+			const isValid = await validateOpenAiKey(key);
+			if (isValid) {
+				const encKey = await encryptOpenAiKey({ openAiKey: key });
+				if (typeof encKey === "string" && !!encKey) {
+					setEncryptedKey(encKey);
+					Toast.success("API key saved locally until logout.");
+				}
+			}
 		} catch (err: any) {
 			const msg = err.message || err.toString();
 			console.error("Error when submitting API KEY", msg);
 			Toast.error(`Error when submitting API KEY : ${msg}`);
 		}
+		setLoading(false);
 	}
 
 	return { encryptedKey: encryptedKey ?? null, submitApiKey };
+}
+
+async function validateOpenAiKey(key: string) {
+	const apiUrl = "https://api.openai.com/v1/engines";
+	const headers = {
+		"Content-Type": "application/json",
+		Authorization: `Bearer ${key}`,
+	};
+
+	try {
+		const response = await fetch(apiUrl, { method: "GET", headers: headers });
+		const data = await response.json();
+		if (!response.ok)
+			throw new Error(`Error: ${response.status}, ${data.error.message}`);
+
+		return true;
+	} catch (error) {
+		console.error("Error:", error.message);
+		Toast.error(error.message);
+		return false;
+	}
 }
