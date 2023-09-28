2b341800b1755db61f62b3cfe3914cdf3de7c751
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2023-09-28 18:58:18 +0530

    feat: use vercel endpoint to fetch HTML of a url Now we use puppeteer to fetch HTML from client side rendering apps instead of JS fetch()

---
+++ apps/api/app/api/get-html/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import puppeteer from "puppeteer";
+
+export async function GET(req: Request) {
+	try {
+		const { searchParams } = new URL(req.url);
+		const hasUrl = searchParams.has("url");
+		const url = hasUrl ? searchParams.get("url") : null;
+		if (!url || !isValidUrl(url)) {
+			return NextResponse.json(
+				{ error: "Invalid or missing URL parameter" },
+				{ status: 400 },
+			);
+		}
+
+		const browser = await puppeteer.launch({ headless: "new" });
+		const page = await browser.newPage();
+		await page.goto(url, { waitUntil: "networkidle2" });
+		const bodyHTML = await page.content();
+		await browser.close();
+
+		return NextResponse.json({ body: bodyHTML }, { status: 200 });
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json({ error: "Error fetching URL" }, { status: 500 });
+	}
+}
+
+function isValidUrl(url: string) {
+	try {
+		new URL(url);
+		return true;
+	} catch (error) {
+		return false;
+	}
+}
--- packages/app/utils/constants.ts
+++ packages/app/utils/constants.ts
@@ -4,3 +4,4 @@ export const isWeb = Platform.OS === "web";
 export const isAndroid = Platform.OS === "android";
 export const isIos = Platform.OS === "ios";
 export const OPENAI_KEY = "OPENAI_KEY";
+export const COOKIED_API_PROD_URL = "https://cookiedapi.vercel.app";
